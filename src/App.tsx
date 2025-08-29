154d91d68e4039580302d8665f962fc708a5b25d
Harikrishnan Ramkumar <1836@trenser.com>
2025-08-29 09:31:24 +0530

    fixed codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
-import { routes } from './routes';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import { routes } from "./routes";
+import config from "../configLoader";
 
 const AppRoutes: React.FC = () => {
   return useRoutes(routes);
 };
 
 const App: React.FC = () => {
+  useEffect(() => {
+    document.title = config.APPLICATION_TITLE || "Default Title";
+  }, []);
+
   return (
     <Router>
       <AppRoutes />
---
+++ src/common.const.ts
@@ -0,0 +1,7 @@
+const CONSTANTS = {
+  EMPTY: "",
+  EMAIL_VALIDATION: /^\S+@\S+\.\S+$/
+
+};
+
+export default CONSTANTS;
